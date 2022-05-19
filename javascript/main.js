1ce6b22f6af822a97947de6f76b670a2d85dac27
s1107777 <s1107777@student.hsleiden.nl>
2022-05-19 17:51:18 +0200

    functions toegevoegd + praat wolk

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,28 @@
-import helloWorld from "./functions.js";
+// import helloWorld from "./functions.js";
 
-helloWorld("YES");
+// helloWorld("YES");
+
+const musicOn = document.getElementById("js--music-on");
+const musicOff = document.getElementById("js--music-off");
+const speakOn = document.getElementById("js--speak-on");
+const speakOff = document.getElementById("js--speak-off");
+
+musicOn.onclick = () =>{
+    musicOff.style.visibility = "visible";
+    musicOn.style.visibility = "hidden";
+};
+
+musicOff.onclick = () =>{
+    musicOff.style.visibility = "hidden";
+    musicOn.style.visibility = "visible";
+};
+
+speakOn.onclick = () =>{
+    speakOff.style.visibility = "visible";
+    speakOn.style.visibility = "hidden";
+};
+
+speakOff.onclick = () =>{
+    speakOff.style.visibility = "hidden";
+    speakOn.style.visibility = "visible";
+};
