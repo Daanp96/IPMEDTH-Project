aba57772b72eaef6ea80843cdbd573dade777d7c
s1107777 <s1107777@student.hsleiden.nl>
2022-05-20 16:37:25 +0200

    begin gemaakt aan js code

--- javascript/main.js
+++ javascript/main.js
@@ -7,6 +7,14 @@ const musicOff = document.getElementById("js--music-off");
 const speakOn = document.getElementById("js--speak-on");
 const speakOff = document.getElementById("js--speak-off");
 
+const startBtn = document.getElementById("js--start-btn")
+const startOKBtn = document.getElementById("js--start-ok-btn");
+const startP = document.getElementById("js--start-p");
+
+const speechBubble = document.getElementById("js--speech-bubble");
+
+let startOK = 0;
+
 musicOn.onclick = () =>{
     musicOff.style.visibility = "visible";
     musicOn.style.visibility = "hidden";
@@ -25,4 +33,24 @@ speakOn.onclick = () =>{
 speakOff.onclick = () =>{
     speakOff.style.visibility = "hidden";
     speakOn.style.visibility = "visible";
+};
+
+// tekstwolk tekst aanpassen.
+startOKBtn.onclick = () => {
+    console.log(startOKBtn.clicked = true);
+
+    if(startOKBtn.clicked == true && startOK == 0){
+        startP.innerHTML = "jaja";
+        startOKBtn.clicked = false; 
+        startOK ++;
+        console.log(startOK);
+    }
+    if (startOKBtn.clicked == true && startOK == 1) {
+        speechBubble.style.visibility = "hidden";
+        startOK ++;
+        console.log(startOK);
+    }
+    if (startOKBtn.clicked == true && startOK == 2) {
+        startBtn.style.visibility = "visible";
+    }
 };
