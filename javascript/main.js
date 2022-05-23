34dc6f2ebae6e407de83969f53e4d5b5ce20dd40
s1107777 <s1107777@student.hsleiden.nl>
2022-05-23 15:54:44 +0200

    startpagina functionaliteiten af

--- javascript/main.js
+++ javascript/main.js
@@ -10,6 +10,7 @@ const speakOff = document.getElementById("js--speak-off");
 const startBtn = document.getElementById("js--start-btn")
 const startOKBtn = document.getElementById("js--start-ok-btn");
 const startP = document.getElementById("js--start-p");
+const startExplaneBtn = document.getElementById("js--start-explane-btn");
 
 const speechBubble = document.getElementById("js--speech-bubble");
 
@@ -35,7 +36,7 @@ speakOff.onclick = () =>{
     speakOn.style.visibility = "visible";
 };
 
-
+// praat wolk
 startOKBtn.onclick = () => {
     console.log(startOK);
 
@@ -45,10 +46,35 @@ startOKBtn.onclick = () => {
         return;
     }
     if(startOK == 1 && true){
+        startExplaneBtn.classList.remove("hide");
+        startExplaneBtn.src = './images/hint-btn.png';
+        startP.innerHTML = 'Klik op het lampje voor een hint.';
+        startOK ++;
+        return;
+    }
+    if(startOK == 2 && true){
+        startExplaneBtn.src = './images/information-btn.png';
+        startP.innerHTML = 'Klik op i voor een informatie.';
+        startOK ++;
+        return;
+    }
+    if(startOK == 3 && true){
+        startExplaneBtn.src = './images/music-on-btn.png';
+        startP.innerHTML = 'Klik op de muzieknoot om de muziek uit te zetten.';
+        startOK ++;
+        return;
+    }
+    if(startOK == 4 && true){
+        startExplaneBtn.src = './images/speak-on-btn.png';
+        startP.innerHTML = 'Klik op de oor om mijn geluid uit te zetten.';
+        startOK ++;
+        return;
+    }
+    if(startOK == 5 && true){
         speechBubble.style.visibility = "hidden";
         startOK ++;
     }
-    if (startOK == 2) {
+    if (startOK == 6) {
         startBtn.style.visibility = "visible";
     }
 };
