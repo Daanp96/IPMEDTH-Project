f7265035b2f5fa0bb88280ab5c3a4f82dbc9e0ec
Daan <daan.passchier@xs4all.nl>
2022-05-26 17:00:13 +0200

    map puzzel drag functie bijgewerkt

--- javascript/functions.js
+++ javascript/functions.js
@@ -41,4 +41,73 @@ const dragDrop = (object, width = '') => {
       };
 }
 
-export default dragDrop;
+const dragDropMap = (obj, width = '', map) => {
+  let currentDroppable = null;
+
+    obj.onmousedown = function(event) {
+      let test = event.target.id;
+      console.log(test);
+
+      let shiftX = event.clientX - obj.getBoundingClientRect().left;
+      let shiftY = event.clientY - obj.getBoundingClientRect().top;
+
+      obj.style.position = 'absolute';
+      obj.style.width = width;
+      obj.style.zIndex = 1000;
+      obj.style.cursor = "url('../images/cursor_grabbing_60.cur'), default";
+      document.body.append(obj);
+
+      moveAt(event.pageX, event.pageY);
+
+      function moveAt(pageX, pageY) {
+        obj.style.left = pageX - shiftX + 'px';
+        obj.style.top = pageY - shiftY + 'px';
+      }
+
+      function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+
+        obj.hidden = true;
+        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+        obj.hidden = false;
+
+        if (!elemBelow) return;
+
+        let droppableBelow = elemBelow.closest('.droppable');
+
+        if (currentDroppable != droppableBelow) {
+          if (currentDroppable) { // null when we were not over a droppable before this event
+            leaveDroppable(currentDroppable);
+          }
+          currentDroppable = droppableBelow;
+          if (currentDroppable) { // null if we're not coming over a droppable now
+            // (maybe just left the droppable)
+            enterDroppable(currentDroppable);
+          }
+        }
+      }
+
+      document.addEventListener('mousemove', onMouseMove);
+
+      obj.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        obj.style.cursor = "url('../images/cursor_grab_60.cur'), default";
+        obj.onmouseup = null;
+      };
+
+    };
+
+    function enterDroppable(elem) {
+      elem.style.opacity = '50%';
+    }
+
+    function leaveDroppable(elem) {
+      elem.style.opacity = '30%';
+    }
+
+    obj.ondragstart = function() {
+      return false;
+    };
+}
+
+export { dragDrop, dragDropMap };
--- javascript/mappuzzel.js
+++ javascript/mappuzzel.js
@@ -1,7 +1,13 @@
-import dragDrop from "./functions.js";
+import {dragDrop, dragDropMap} from "./functions.js";
 
-const slide = document.getElementsByClassName("drag")
+const slide = document.getElementsByClassName("drag");
+const map = document.getElementsByClassName("droppable");
 
 for (let i = 0; i < slide.length; i++) {
-    dragDrop(slide[i], '290px');
+
+    dragDropMap(slide[i], '240px', map);
+
+    // if(sources == map_sources) {
+    //     console.log("yes");
+    // }
 }
