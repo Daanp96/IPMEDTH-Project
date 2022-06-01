05682e8c97f30baa16750f3c80a3dc01830db929
s1107777 <s1107777@student.hsleiden.nl>
2022-06-01 17:01:14 +0200

    begin gemaakt aan dierentuinpad

---
+++ javascript/dierentuinpad.js
@@ -0,0 +1,10 @@
+const bird = document.getElementById("js--bird");
+
+bird.addEventListener("click", function() {
+    console.log("f");
+    startRekensomPuzzel();
+});
+
+function startRekensomPuzzel(){
+    window.location.href="rekensom.html";  
+  };
