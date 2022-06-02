e3fb12f77459aa048af9bb72ec5d3039badd9aab
Daan <daan.passchier@xs4all.nl>
2022-06-02 11:22:09 +0200

    new function arctic dragdrop

--- public/javascript/functions.js
+++ public/javascript/functions.js
@@ -130,6 +130,49 @@ const dragDropMap = (obj, width = '', btn, zookpr, speech) => {
     };
 }
 
+const dragDropArctic = (object, width = '') => {
+
+  object.onmousedown = (event) => {
+
+      let shiftX = event.clientX - object.getBoundingClientRect().left;
+      let shiftY = event.clientY - object.getBoundingClientRect().top;
+    
+      object.style.position = 'absolute';
+      object.style.width = width;
+      object.style.zIndex = 1000;
+      object.style.cursor = "url('../images/cursor_grabbing_60.cur'), default";
+      document.body.append(object);
+    
+      moveAt(event.pageX, event.pageY);
+    
+      // moves the object at (pageX, pageY) coordinates
+      // taking initial shifts into account
+      function moveAt(pageX, pageY) {
+        object.style.left = pageX - shiftX + 'px';
+        object.style.top = pageY - shiftY + 'px';
+      }
+    
+      function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+      }
+    
+      // move the object on mousemove
+      document.addEventListener('mousemove', onMouseMove);
+    
+      // drop the object, remove unneeded handlers
+      object.onmouseup = () => {
+        document.removeEventListener('mousemove', onMouseMove);
+        object.style.cursor = "url('../images/cursor_grab_60.cur'), default";
+        object.onmouseup = null;
+      };
+    
+    };
+    
+    object.ondragstart = () => {
+      return false;
+    };
+}
+
 const modalView = (modal, title, text, button, {
     title_content = "",
     text_content = "",
