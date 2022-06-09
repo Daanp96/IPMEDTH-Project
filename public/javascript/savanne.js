24169d9cb6d903672b13d0b804cb79504a128ee5
s1107777 <s1107777@student.hsleiden.nl>
2022-06-09 19:36:38 +0200

    hint bug fix + button aangepast

--- public/javascript/savanne.js
+++ public/javascript/savanne.js
@@ -35,6 +35,7 @@ hintBtn.onclick = () => {
                 speechBubble.style.visibility = "hidden";
                 speechBubble_p.style.visibility = "hidden";
                 startOKBtn.style.visibility = "hidden";
+                countHint++;
             }
             break;
         case 1: 
@@ -50,6 +51,7 @@ hintBtn.onclick = () => {
                 explaineBtn.classList.add("hide");
                 speechBubble_p.style.visibility = "hidden";
                 startOKBtn.style.visibility = "hidden";
+                countHint++;
             }
             break;
         case 2: 
@@ -66,6 +68,7 @@ hintBtn.onclick = () => {
                 explaineBtn.classList.add("hide");
                 speechBubble_p.style.visibility = "hidden";
                 startOKBtn.style.visibility = "hidden";
+                countHint++;
             }
             break;
         case 3: 
@@ -78,16 +81,18 @@ hintBtn.onclick = () => {
                 speechBubble.style.visibility = "hidden";
                 speechBubble_p.style.visibility = "hidden";
                 startOKBtn.style.visibility = "hidden";
+                countHint++;
             }
             break;
         case 4:
             speechBubble.style.visibility = "hidden";
             startOKBtn.style.visibility = "hidden";
+            countHint++;
             break;
     }
     speechBubble_p.innerHTML = tekst;
     explaineBtn.src = image;
-    countHint++;
+    
 };
 
 function endRekensomPuzzel(){
