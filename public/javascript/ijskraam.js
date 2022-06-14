f3f81e74ecc8374abe37b8a414af8b8004a47ee1
Daan <daan.passchier@xs4all.nl>
2022-06-14 16:00:09 +0200

    nieuwe ijskraam

--- public/javascript/ijskraam.js
+++ public/javascript/ijskraam.js
@@ -1,11 +1,11 @@
-const ijskraam = document.getElementById("js--ijs-kraam");
+const ijsbalie = document.getElementById("js--balie");
 const overlay = document.getElementById("js--overlay");
 const ijsjes = document.getElementsByClassName("ijs");
 
-ijskraam.onclick = (e) => {
+ijsbalie.onclick = (e) => {
     overlay.style.opacity =  "1";
     overlay.style.zIndex = "1";
-    ijskraam.classList.remove("ijs_animatie");
+    ijsbalie.classList.remove("ijs_animatie");
     for (let ijs of ijsjes) {
         ijs.style.opacity = "1";
         ijs.style.zIndex = "5";
