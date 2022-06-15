d594ee85185791d6a2bb4eb2d31ff081a319b9b9
s1107777 <s1107777@student.hsleiden.nl>
2022-06-15 11:57:20 +0200

    drag&drop toegepast aan eerste element

--- public/javascript/functions.js
+++ public/javascript/functions.js
@@ -1,7 +1,7 @@
 let layedPieces = 0;
 let pinguinsFed = 0;
 
-const dragDrop = (object, width = '') => {
+const dragDrop = (object) => {
 
     object.onmousedown = (event) => {
 
@@ -9,7 +9,6 @@ const dragDrop = (object, width = '') => {
         let shiftY = event.clientY - object.getBoundingClientRect().top;
       
         object.style.position = 'absolute';
-        object.style.width = width;
         object.style.zIndex = 1000;
         object.style.cursor = "url('../images/cursor_grabbing_60.cur'), default";
         document.body.append(object);
@@ -251,4 +250,52 @@ const textBubble = () => {
   }
 }
 
-export { dragDrop, dragDropMap, dragDropArctic, modalView};
+const dragDropGiraffe = (object) => {
+
+  object.onmousedown = (event) => {
+
+    let shiftX = event.clientX - object.getBoundingClientRect().left;
+    let shiftY = event.clientY - object.getBoundingClientRect().top;
+    
+    object.style.position = 'absolute';
+    object.style.zIndex = 10;
+    object.style.cursor = "url('../images/cursor_grabbing_60.cur'), default";
+    object.style.height = "50%";
+    object.style.width = "auto";
+    document.body.append(object);
+    
+    console.log(object);
+   
+    moveAt(event.pageX, event.pageY);
+    
+    // moves the object at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+      object.style.left = pageX - shiftX + 'px';
+      object.style.top = pageY - shiftY + 'px';
+    }
+    
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+
+    }
+    
+    // move the object on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+    
+    // drop the object, remove unneeded handlers
+    document.onmouseup = () => {
+      document.removeEventListener('mousemove', onMouseMove);
+      object.style.cursor = "url('../images/cursor_grab_60.cur'), default";
+      object.onmouseup = null;
+      
+    };
+    
+  };
+    
+  object.ondragstart = () => {
+    return false;
+  };
+}
+
+export { dragDrop, dragDropMap, dragDropArctic, modalView, dragDropGiraffe};
--- public/javascript/giraffe.js
+++ public/javascript/giraffe.js
@@ -1,19 +1,39 @@
-const introBtn = document.getElementById("js--intro-btn");
-const sectionIntro = document.getElementById("js--article-intro");
-const sectionEnclosure = document.getElementById("js--article-enclosure");
+import {dragDropGiraffe} from "./functions.js";
 
+const introBtn = document.getElementById("js--intro-btn");
+const articleIntro = document.getElementById("js--article-intro");
+const articleEnclosure = document.getElementById("js--article-enclosure");
+// const enclosureField = document.getElementById("js--enclosure-field");
+
+//verschillende sidebars
 const sidebar = document.getElementById("js--sidebar");
 const treePopup = document.getElementById("js--tree-popup");
 
+//buttons van begin sidebar
 const treeBtn = document.getElementById("js--tree-button");
 
+//buttons sidebar tree
+const sidebarTree1 = document.getElementById("js--sidebar-tree-1");
+
+//objecten
+const tree1 = document.getElementById("js--tree-1");
+
+dragDropGiraffe(tree1);
+
 introBtn.onclick = () => {
-    sectionIntro.style.display = "none";
-    sectionEnclosure.style.display = "block";
+    articleIntro.style.display = "none";
+    articleEnclosure.style.display = "block";
 };
 
 treeBtn.onclick = () => {
-    treePopup.classList.remove("tree");
+    treePopup.classList.remove("tree-sidebar");
     treePopup.classList.add("slidein-from-left");
     sidebar.style.display = "none";
 }
+
+sidebarTree1.onclick = () => {
+    tree1.classList.remove("hide");
+    sidebar.style.display = "";
+    treePopup.classList.add("tree-sidebar");
+    sidebar.classList.add("slidein-from-left");
+};
