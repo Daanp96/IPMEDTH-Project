4a429fef208843b41bf9af70ccdd53acec50a45f
s1107777 <s1107777@student.hsleiden.nl>
2022-06-30 12:06:43 +0200

    merge

--- public/javascript/savanne.js
+++ public/javascript/savanne.js
@@ -1,3 +1,4 @@
+import {reloadSpeech, reloadHint} from "./functions.js";
 const text = "olifant";
 
 const speechBubble = document.getElementById("js--speech-bubble");
@@ -10,11 +11,13 @@ const speakOn = document.getElementById("js--speak-on");
 const speakOff = document.getElementById("js--speak-off");
 
 const mapOverlay = document.getElementById("js--map-overlay");
+const herhaal = document.getElementById("js--speech-reload");
 
 let startOK = 0;
 let countHint = 0;
 let tekst = '';
 let image = '';
+let countHerhaal = 0;
 
 // const hint1 = new Audio("../audio/6-aap/hint-1.mp3");
 // const hint2 = new Audio("../audio/6-aap/hint-2.mp3");
@@ -28,6 +31,10 @@ const hint1 = new Audio("../audio/Tjalle/6-savanne/hint-1.m4a");
 const hint2 = new Audio("../audio/Tjalle/6-savanne/hint-2.m4a");
 const hint3 = new Audio("../audio/Tjalle/6-savanne/hint-3.m4a");
 const hint4 = new Audio("../audio/Tjalle/6-savanne/hint-4.m4a");
+
+const audioHerhaalSavanne = [savanneVerblijf, verbeteren];
+const hintHerhaalSavanne = [hint1, hint2, hint3, hint4];
+let isHint = false;
 
 setInterval(() => {
     if (localStorage.getItem("speakOnStorage") == 'hidden') {
@@ -67,32 +74,20 @@ speakOff.onclick = () => {
     speakOffFunction();
 };
 
-// speakOn.onclick = () => {
-//     speakOff.style.visibility = "visible";
-//     speakOn.style.visibility = "hidden";
-//     savanneVerblijf.muted = true;
-//     verbeteren.muted = true;
-//     hint1.muted = true;
-//     hint2.muted = true;
-//     hint3.muted = true;
-//     hint4.muted = true;
-// };
-
-// speakOff.onclick = () => {
-//     speakOff.style.visibility = "hidden";
-//     speakOn.style.visibility = "visible";
-//     savanneVerblijf.muted = false;
-//     verbeteren.muted = false;
-//     hint1.muted = false;
-//     hint2.muted = false;
-//     hint3.muted = false;
-//     hint4.muted = false;
-// };
 
 hintBtn.disabled = true;
 savanneVerblijf.play();
 savanneVerblijf.onended = () => {
     startOKBtn.style.display = "block";
+    herhaal.style.display = "block";
+}
+
+herhaal.onclick = () => {
+    if(isHint) {
+        reloadHint(hintHerhaalSavanne[countHint], herhaal);
+    } else {
+        reloadSpeech(audioHerhaalSavanne[countHerhaal], herhaal);
+    }
 }
 
 $(".popup__container-typen--textbox").on("focus blur", function(){
@@ -143,6 +138,7 @@ startOKBtn.onclick = () => {
             verbeteren.play();
             verbeteren.onended = () => {
                 startOKBtn.style.display = "block";
+                herhaal.style.display = "block";
             }
             break;
         case 1: 
@@ -152,18 +148,17 @@ startOKBtn.onclick = () => {
             hintBtn.disabled = false;
             break;
     }
-
+    herhaal.style.display = "none";
     startOKBtn.style.display = "none";
     speechBubble_p.innerHTML = tekst;
     explaineBtn.src = image;
     startOK++;
-    // speechBubble.style.visibility = "hidden";
-    // speechBubble.style.zIndex = "";
-    // mapOverlay.style.zIndex = "-1";
+    countHerhaal++;
+
 };
 
 hintBtn.onclick = () => {
-    console.log(countHint);
+    isHint = true;
     switch (countHint) {
         case 0: 
             speechBubble.style.visibility = "visible";
@@ -173,6 +168,7 @@ hintBtn.onclick = () => {
             hint1.play();
             hint1.onended = () => {
                 startOKBtn.style.display = "block";
+                herhaal.style.display = "block";
             }
 
             startOKBtn.onclick = () => {
@@ -180,6 +176,7 @@ hintBtn.onclick = () => {
                 speechBubble_p.style.visibility = "hidden";
                 // startOKBtn.style.visibility = "hidden";
                 countHint++;
+                isHint = false;
             }
             break;
         case 1: 
@@ -190,15 +187,17 @@ hintBtn.onclick = () => {
             hint2.play();
             hint2.onended = () => {
                 startOKBtn.style.display = "block";
+                herhaal.style.display = "block";
             }
             startOKBtn.onclick = () => {
                 speechBubble.style.visibility = "hidden";
                 speechBubble_p.style.visibility = "hidden";
                 // startOKBtn.style.visibility = "hidden";
                 countHint++;
+                isHint = false;
             }
             break;
-        case 2: 
+            case 2: 
             speechBubble.style.visibility = "visible";
             explaineBtn.classList.remove("hide");
             speechBubble_p.style.visibility = "visible";
@@ -208,12 +207,14 @@ hintBtn.onclick = () => {
             hint3.play();
             hint3.onended = () => {
                 startOKBtn.style.display = "block";
+                herhaal.style.display = "block";
             }
-            image = '../images/savanne/keyboard2.png';
+            image = '../images/savanne/keyboard.png';
             startOKBtn.onclick = () => {
                 speechBubble.style.visibility = "hidden";
                 explaineBtn.classList.add("hide");
                 speechBubble_p.style.visibility = "hidden";
+                isHint = false;
                 // startOKBtn.style.visibility = "hidden";
                 countHint++;
             }
@@ -226,11 +227,13 @@ hintBtn.onclick = () => {
             hint4.play();
             hint4.onended = () => {
                 startOKBtn.style.display = "block";
+                herhaal.style.display = "block";
             }
             startOKBtn.onclick = () => {
                 speechBubble.style.visibility = "hidden";
                 speechBubble_p.style.visibility = "hidden";
                 startOKBtn.style.visibility = "hidden";
+                isHint = false;
                 countHint++;
             }
             break;
@@ -241,7 +244,8 @@ hintBtn.onclick = () => {
             countHint++;
             break;
     }
-
+    
+    herhaal.style.display = "none";
     startOKBtn.style.display = "none";
     speechBubble_p.innerHTML = tekst;
     explaineBtn.src = image;
