13ff4dd2d520dd1eb90076cfbf5119e11b65dd6c
s1107777 <s1107777@student.hsleiden.nl>
2022-07-01 16:15:44 +0200

    punten aftrek na gebruik hints

--- public/javascript/pinguinverblijf.js
+++ public/javascript/pinguinverblijf.js
@@ -30,34 +30,10 @@ const hint2 = new Audio("../audio/Tjalle/7-pinguins/hint-2.m4a");
 
 const hintHerhaalArctic = [hint1, hint2];
 let isHint = false;
+let startTime;
 
-hintBtn.disabled = true;
-pinguinVerblijf.play();
-pinguinVerblijf.onplaying = () => {
-    mouthMove.style.display = "block";
-    mouthMove.classList.add("mouth_move_verblijf");
-}
-pinguinVerblijf.onended = () => {
-    mouthMove.style.display = "none";
-    startOKBtn.style.display = "flex";
-    herhaal.style.display = "block";
-}
-
-startOKBtn.onclick = () => {
-    speechBubble.style.visibility = "hidden";
-    fishDrag.style.visibility ="visible";
-    // startOKBtn.style.visibility = "hidden";
-    speechBubble.style.zIndex = "";
-    mapOverlay.style.zIndex = "-1";
-    hintBtn.disabled = false;
-    if(countHint == 0){
-        hintGlow(60000, hintBtnGlow);
-        setTimeout(() => {
-            hintBtnGlow.classList.remove("glow");
-        }, 70000);
-    } else {
-        hintBtnGlow.classList.remove("glow");
-    }
+function start(){
+    startTime = new Date();
 }
 
 setInterval(() => {
@@ -86,93 +62,126 @@ function speakOffFunction(){
     hint1.muted = false;
     hint2.muted = false;
 };
-  
-speakOn.onclick = () => {
-    speakOnFunction();
-};
-speakOff.onclick = () => {
-    speakOffFunction();
-};
-
-herhaal.onclick = () => {
-    if(isHint) {
-        reloadHint(hintHerhaalArctic[countHint], herhaal);
-    } else {
-        reloadSpeech(pinguinVerblijf, herhaal);
-    }
-}
-
-pinguinverblijfBtn.onclick = () => {
-    window.location.href = "./dierentuinpad.html";
-}
-
 dragDropArctic(fishDrag, speechBubble, speechBubble_p, pinguinverblijfBtn, startOKBtn, goedGedaan);
 
-hintBtn.onclick = () => {
-    isHint = true;
-    console.log(countHint);
-    switch (countHint) {
-        case 0: 
+if(document.URL.includes("pinguinverblijf.html") ){
+    hintBtn.disabled = true;
+    pinguinVerblijf.play();
+    pinguinVerblijf.onplaying = () => {
+        mouthMove.style.display = "block";
+        mouthMove.classList.add("mouth_move_verblijf");
+    }
+    pinguinVerblijf.onended = () => {
+        mouthMove.style.display = "none";
+        startOKBtn.style.display = "flex";
+        herhaal.style.display = "block";
+    }
+    
+    startOKBtn.onclick = () => {
+        speechBubble.style.visibility = "hidden";
+        fishDrag.style.visibility ="visible";
+        // startOKBtn.style.visibility = "hidden";
+        speechBubble.style.zIndex = "";
+        mapOverlay.style.zIndex = "-1";
+        hintBtn.disabled = false;
+        start();
+        if(countHint == 0){
+            hintGlow(60000, hintBtnGlow);
+            setTimeout(() => {
+                hintBtnGlow.classList.remove("glow");
+            }, 70000);
+        } else {
             hintBtnGlow.classList.remove("glow");
-            speechBubble.style.visibility = "visible";
-            // explaineBtn.classList.remove("hide");
-            speechBubble_p.style.visibility = "visible";
-            // startOKBtn.style.visibility = "visible";
-            tekst = 'Alle pinguïns moeten gevoed worden.';
-            hint1.play();
-            hint1.onplaying = () => {
-                mouthMove.style.display = "block";
-            }
-            hint1.onended = () => {
-                mouthMove.style.display = "none";
-                startOKBtn.style.display = "flex";
-                herhaal.style.display = "block";
-            }
-            startOKBtn.onclick = () => {
-                speechBubble.style.visibility = "hidden";
-                explaineBtn.classList.add("hide");
-                speechBubble_p.style.visibility = "hidden";
-                isHint = false;
-                // startOKBtn.style.visibility = "hidden";
-                countHint++;
-            }
-            break;
-        case 1: 
-            speechBubble.style.visibility = "visible";
-            explaineBtn.classList.remove("hide");
-            speechBubble_p.style.visibility = "visible";
-            // startOKBtn.style.visibility = "visible";
-            tekst = 'Beweeg je muis naar de emmer met de vis en doe dit:';
-            hint2.play();
-            hint2.onplaying = () => {
-                mouthMove.style.display = "block";
-            }
-            hint2.onended = () => {
-                mouthMove.style.display = "none";
-                startOKBtn.style.display = "flex";
-                herhaal.style.display = "block";
-            }
-            image = '../images/gif/klik_zijkant.gif';
-            startOKBtn.onclick = () => {
-                speechBubble.style.visibility = "hidden";
-                explaineBtn.classList.add("hide");
-                speechBubble_p.style.visibility = "hidden";
-                startOKBtn.style.visibility = "hidden";
-                isHint = false;
-                countHint++;
-            }
-            break;
-        case 2:
-            speechBubble.style.visibility = "hidden";
-            startOKBtn.style.display = "none";
-            speechBubble.style.zIndex = "";
-            mapOverlay.style.zIndex = "-1";
-            break;
+        }
     }
 
-    herhaal.style.display = "none";
-    startOKBtn.style.display = "none";
-    speechBubble_p.innerHTML = tekst;
-    explaineBtn.src = image;
+    speakOn.onclick = () => {
+        speakOnFunction();
+    };
+    speakOff.onclick = () => {
+        speakOffFunction();
+    };
     
-};
+    herhaal.onclick = () => {
+        if(isHint) {
+            reloadHint(hintHerhaalArctic[countHint], herhaal);
+        } else {
+            reloadSpeech(pinguinVerblijf, herhaal);
+        }
+    }
+    
+    pinguinverblijfBtn.onclick = () => {
+        window.location.href = "./dierentuinpad.html";
+    }
+        
+    hintBtn.onclick = () => {
+        isHint = true;
+        console.log(countHint);
+        switch (countHint) {
+            case 0: 
+                hintBtnGlow.classList.remove("glow");
+                speechBubble.style.visibility = "visible";
+                // explaineBtn.classList.remove("hide");
+                speechBubble_p.style.visibility = "visible";
+                // startOKBtn.style.visibility = "visible";
+                tekst = 'Alle pinguïns moeten gevoed worden.';
+                hint1.play();
+                hint1.onplaying = () => {
+                    mouthMove.style.display = "block";
+                }
+                hint1.onended = () => {
+                    mouthMove.style.display = "none";
+                    startOKBtn.style.display = "flex";
+                    herhaal.style.display = "block";
+                }
+                startOKBtn.onclick = () => {
+                    speechBubble.style.visibility = "hidden";
+                    explaineBtn.classList.add("hide");
+                    speechBubble_p.style.visibility = "hidden";
+                    isHint = false;
+                    // startOKBtn.style.visibility = "hidden";
+                    countHint++;
+                }
+                break;
+            case 1: 
+                speechBubble.style.visibility = "visible";
+                explaineBtn.classList.remove("hide");
+                speechBubble_p.style.visibility = "visible";
+                // startOKBtn.style.visibility = "visible";
+                tekst = 'Beweeg je muis naar de emmer met de vis en doe dit:';
+                hint2.play();
+                hint2.onplaying = () => {
+                    mouthMove.style.display = "block";
+                }
+                hint2.onended = () => {
+                    mouthMove.style.display = "none";
+                    startOKBtn.style.display = "flex";
+                    herhaal.style.display = "block";
+                }
+                image = '../images/gif/klik_zijkant.gif';
+                startOKBtn.onclick = () => {
+                    speechBubble.style.visibility = "hidden";
+                    explaineBtn.classList.add("hide");
+                    speechBubble_p.style.visibility = "hidden";
+                    startOKBtn.style.visibility = "hidden";
+                    isHint = false;
+                    countHint++;
+                }
+                break;
+            case 2:
+                speechBubble.style.visibility = "hidden";
+                startOKBtn.style.display = "none";
+                speechBubble.style.zIndex = "";
+                mapOverlay.style.zIndex = "-1";
+                break;
+        }
+    
+        herhaal.style.display = "none";
+        startOKBtn.style.display = "none";
+        speechBubble_p.innerHTML = tekst;
+        explaineBtn.src = image;
+        
+    };
+}
+  
+export { countHint, startTime};
--- public/javascript/savanne.js
+++ public/javascript/savanne.js
@@ -15,16 +15,6 @@ const mapOverlay = document.getElementById("js--map-overlay");
 const herhaal = document.getElementById("js--speech-reload");
 const mouthMove = document.getElementById("js--mouth");
 
-let startOK = 0;
-let countHint = 0;
-let tekst = '';
-let image = '';
-let countHerhaal = 0;
-
-// const hint1 = new Audio("../audio/6-aap/hint-1.mp3");
-// const hint2 = new Audio("../audio/6-aap/hint-2.mp3");
-// const hint3 = new Audio("../audio/6-aap/hint-3.mp3");
-// const hint4 = new Audio("../audio/6-aap/hint-4.mp3");
 
 const savanneVerblijf = new Audio("../audio/Tjalle/6-savanne/1-savanneverblijf.m4a");
 const verbeteren = new Audio("../audio/Tjalle/6-savanne/2-verbeteren.m4a");
@@ -36,8 +26,19 @@ const hint4 = new Audio("../audio/Tjalle/6-savanne/hint-4.m4a");
 
 const audioHerhaalSavanne = [savanneVerblijf, verbeteren];
 const hintHerhaalSavanne = [hint1, hint2, hint3, hint4];
+
+
+let startOK = 0;
+let countHint = 0;
+let tekst = '';
+let image = '';
+let countHerhaal = 0;
 let isHint = false;
-hintBtn.disabled = true;
+let startTime;
+
+function start(){
+    startTime = new Date();
+}
 
 setInterval(() => {
     if (localStorage.getItem("speakOnStorage") == 'hidden') {
@@ -69,233 +70,234 @@ function speakOffFunction(){
     hint3.muted = false;
     hint4.muted = false;
 };
-  
-speakOn.onclick = () => {
-    speakOnFunction();
-};
-speakOff.onclick = () => {
-    speakOffFunction();
-};
 
 
-savanneVerblijf.play();
-savanneVerblijf.onplaying = () => {
-    mouthMove.style.display = "block";
-    mouthMove.classList.add("mouth_move_verblijf");
-}
-savanneVerblijf.onended = () => {
-    mouthMove.style.display = "none";
-    startOKBtn.style.display = "block";
-    herhaal.style.display = "block";
-}
 
-herhaal.onclick = () => {
-    if(isHint) {
-        reloadHint(hintHerhaalSavanne[countHint], herhaal);
-    } else {
-        reloadSpeech(audioHerhaalSavanne[countHerhaal], herhaal);
+if(document.URL.includes("savanne.html") ){
+    hintBtn.disabled = true;
+
+    speakOn.onclick = () => {
+        speakOnFunction();
+    };
+    speakOff.onclick = () => {
+        speakOffFunction();
+    };
+    
+    savanneVerblijf.play();
+    savanneVerblijf.onplaying = () => {
+        mouthMove.style.display = "block";
+        mouthMove.classList.add("mouth_move_verblijf");
     }
-}
-
-$(".popup__container-savanne--textbox").on("focus blur", function(){
-    $(".popup__container-savanne--wrapper").toggleClass("focused");
-});
-
-$(".popup__container-savanne--wrapper").click(function (e) {
-    if (e.target == this) {
-        var b = $(".popup__container-savanne--textbox", this).focus();
-        setEndOfContenteditable(b[0]);
+    savanneVerblijf.onended = () => {
+        mouthMove.style.display = "none";
+        startOKBtn.style.display = "block";
+        herhaal.style.display = "block";
     }
-}).trigger("click");
+    
+    herhaal.onclick = () => {
+        if(isHint) {
+            reloadHint(hintHerhaalSavanne[countHint], herhaal);
+        } else {
+            reloadSpeech(audioHerhaalSavanne[countHerhaal], herhaal);
+        }
+    }    
 
-$(".popup__container-savanne--wrapper > .popup__container-savanne--textbox").on("input", function(){
-    var ipt = $(this).text().replace(/\u00A0/g, " ");
-    //freakin NO-BREAK SPACE needs extra care
-    if(text.indexOf(ipt) == 0){
-        $(".popup__container-savanne--textbox--gray").text(text.substr(ipt.length, text.length));
-    }else{
-        $(".popup__container-savanne--textbox--gray").text("");
+    $(".popup__container-savanne--textbox").on("focus blur", function(){
+        $(".popup__container-savanne--wrapper").toggleClass("focused");
+    });
+    
+    $(".popup__container-savanne--wrapper").click(function (e) {
+        if (e.target == this) {
+            var b = $(".popup__container-savanne--textbox", this).focus();
+            setEndOfContenteditable(b[0]);
+        }
+    }).trigger("click");
+    
+    $(".popup__container-savanne--wrapper > .popup__container-savanne--textbox").on("input", function(){
+        var ipt = $(this).text().replace(/\u00A0/g, " ");
+        //freakin NO-BREAK SPACE needs extra care
+        if(text.indexOf(ipt) == 0){
+            $(".popup__container-savanne--textbox--gray").text(text.substr(ipt.length, text.length));
+        }else{
+            $(".popup__container-savanne--textbox--gray").text("");
+        }
+    }).trigger("input");
+    
+    
+    function setEndOfContenteditable(contentEditableElement) {
+        var range, selection;
+        if (document.createRange) {
+            range = document.createRange();
+            range.selectNodeContents(contentEditableElement);
+            range.collapse(false);
+            selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(range);
+        } else if (document.selection) {
+            range = document.body.createTextRange();
+            range.moveToElementText(contentEditableElement);
+            range.collapse(false);
+            range.select();
+        }
     }
-}).trigger("input");
-
-
-function setEndOfContenteditable(contentEditableElement) {
-    var range, selection;
-    if (document.createRange) {
-        range = document.createRange();
-        range.selectNodeContents(contentEditableElement);
-        range.collapse(false);
-        selection = window.getSelection();
-        selection.removeAllRanges();
-        selection.addRange(range);
-    } else if (document.selection) {
-        range = document.body.createTextRange();
-        range.moveToElementText(contentEditableElement);
-        range.collapse(false);
-        range.select();
-    }
-}
-
-
-startOKBtn.onclick = () => {
-    switch(startOK) {
-        case 0:
-            tekst = 'Maar blijkbaar hebben we de verkeerde naam opgehangen! Kan jij het bord voor ons verbeteren?';
-            image = '';
-            verbeteren.play();
-            verbeteren.onplaying = () => {
-                mouthMove.style.display = "block";
-                mouthMove.classList.add("mouth_move_verblijf");
-            }
-            verbeteren.onended = () => {
-                mouthMove.style.display = "none";
-                startOKBtn.style.display = "block";
-                herhaal.style.display = "block";
-            }
-            break;
-        case 1: 
-            speechBubble.style.visibility = "hidden";
-            speechBubble.style.zIndex = "";
-            mapOverlay.style.zIndex = "-1";
-            hintBtn.disabled = false;
-            if(countHint == 0){
-                console.log("tellen2");
-                hintGlow(60000, hintBtnGlow);
-                setTimeout(() => {
+    
+    startOKBtn.onclick = () => {
+        switch(startOK) {
+            case 0:
+                tekst = 'Maar blijkbaar hebben we de verkeerde naam opgehangen! Kan jij het bord voor ons verbeteren?';
+                image = '';
+                verbeteren.play();
+                verbeteren.onplaying = () => {
+                    mouthMove.style.display = "block";
+                    mouthMove.classList.add("mouth_move_verblijf");
+                }
+                verbeteren.onended = () => {
+                    mouthMove.style.display = "none";
+                    startOKBtn.style.display = "block";
+                    herhaal.style.display = "block";
+                }
+                break;
+            case 1: 
+                speechBubble.style.visibility = "hidden";
+                speechBubble.style.zIndex = "";
+                mapOverlay.style.zIndex = "-1";
+                hintBtn.disabled = false;
+                start();
+                if(countHint == 0){
+                    hintGlow(60000, hintBtnGlow);
+                    setTimeout(() => {
+                        hintBtnGlow.classList.remove("glow");
+                    }, 70000);
+                } else {
                     hintBtnGlow.classList.remove("glow");
-                }, 70000);
-            } else {
+                }
+                break;
+        }
+        herhaal.style.display = "none";
+        startOKBtn.style.display = "none";
+        speechBubble_p.innerHTML = tekst;
+        explaineBtn.src = image;
+        startOK++;
+        countHerhaal++;
+    
+    };
+    
+    hintBtn.onclick = () => {
+        isHint = true;
+        switch (countHint) {
+            case 0: 
                 hintBtnGlow.classList.remove("glow");
-            }
-            break;
-    }
-    herhaal.style.display = "none";
-    startOKBtn.style.display = "none";
-    speechBubble_p.innerHTML = tekst;
-    explaineBtn.src = image;
-    startOK++;
-    countHerhaal++;
-
-};
-
-hintBtn.onclick = () => {
-    isHint = true;
-    switch (countHint) {
-        case 0: 
-            hintBtnGlow.classList.remove("glow");
-
-            speechBubble.style.visibility = "visible";
-            speechBubble_p.style.visibility = "visible";
-            // startOKBtn.style.visibility = "visible";
-            tekst = 'Klik op het bord.';
-            hint1.play();
-            hint1.onplaying = () => {
-                mouthMove.style.display = "block";
-                mouthMove.classList.add("mouth_move_verblijf");
-            }
-            hint1.onended = () => {
-                mouthMove.style.display = "none";
-                startOKBtn.style.display = "block";
-                herhaal.style.display = "block";
-            }
-
-            startOKBtn.onclick = () => {
-                speechBubble.style.visibility = "hidden";
-                speechBubble_p.style.visibility = "hidden";
-                // startOKBtn.style.visibility = "hidden";
-                countHint++;
-                isHint = false;
-            }
-            break;
-        case 1: 
-            speechBubble.style.visibility = "visible";
-            speechBubble_p.style.visibility = "visible";
-            // startOKBtn.style.visibility = "visible";
-            tekst = 'Klik op de foute naam.';
-            hint2.play();
-            hint2.onplaying = () => {
-                mouthMove.style.display = "block";
-                mouthMove.classList.add("mouth_move_verblijf");
-            }
-            hint2.onended = () => {
-                mouthMove.style.display = "none";
-                startOKBtn.style.display = "block";
-                herhaal.style.display = "block";
-            }
-            startOKBtn.onclick = () => {
-                speechBubble.style.visibility = "hidden";
-                speechBubble_p.style.visibility = "hidden";
-                // startOKBtn.style.visibility = "hidden";
-                countHint++;
-                isHint = false;
-            }
-            break;
+    
+                speechBubble.style.visibility = "visible";
+                speechBubble_p.style.visibility = "visible";
+                // startOKBtn.style.visibility = "visible";
+                tekst = 'Klik op het bord.';
+                hint1.play();
+                hint1.onplaying = () => {
+                    mouthMove.style.display = "block";
+                    mouthMove.classList.add("mouth_move_verblijf");
+                }
+                hint1.onended = () => {
+                    mouthMove.style.display = "none";
+                    startOKBtn.style.display = "block";
+                    herhaal.style.display = "block";
+                }
+    
+                startOKBtn.onclick = () => {
+                    speechBubble.style.visibility = "hidden";
+                    speechBubble_p.style.visibility = "hidden";
+                    // startOKBtn.style.visibility = "hidden";
+                    countHint++;
+                    isHint = false;
+                }
+                break;
+            case 1: 
+                speechBubble.style.visibility = "visible";
+                speechBubble_p.style.visibility = "visible";
+                // startOKBtn.style.visibility = "visible";
+                tekst = 'Klik op de foute naam.';
+                hint2.play();
+                hint2.onplaying = () => {
+                    mouthMove.style.display = "block";
+                    mouthMove.classList.add("mouth_move_verblijf");
+                }
+                hint2.onended = () => {
+                    mouthMove.style.display = "none";
+                    startOKBtn.style.display = "block";
+                    herhaal.style.display = "block";
+                }
+                startOKBtn.onclick = () => {
+                    speechBubble.style.visibility = "hidden";
+                    speechBubble_p.style.visibility = "hidden";
+                    // startOKBtn.style.visibility = "hidden";
+                    countHint++;
+                    isHint = false;
+                }
+                break;
             case 2: 
-            speechBubble.style.visibility = "visible";
-            explaineBtn.classList.remove("hide");
-            speechBubble_p.style.visibility = "visible";
-            // startOKBtn.style.visibility = "visible";
-            explaineBtn.style.width = "23vw";
-            tekst = 'Je toetsenbord gebruikt blokletters. Dit zijn de kleine letters.';
-            hint3.play();
-            hint3.onplaying = () => {
-                mouthMove.style.display = "block";
-                mouthMove.classList.add("mouth_move_verblijf");
-            }
-            hint3.onended = () => {
-                mouthMove.style.display = "none";
-                startOKBtn.style.display = "block";
-                herhaal.style.display = "block";
-            }
-            image = '../images/savanne/keyboard.png';
-            startOKBtn.onclick = () => {
+                speechBubble.style.visibility = "visible";
+                explaineBtn.classList.remove("hide");
+                speechBubble_p.style.visibility = "visible";
+                // startOKBtn.style.visibility = "visible";
+                explaineBtn.style.width = "23vw";
+                tekst = 'Je toetsenbord gebruikt blokletters. Dit zijn de kleine letters.';
+                hint3.play();
+                hint3.onplaying = () => {
+                    mouthMove.style.display = "block";
+                    mouthMove.classList.add("mouth_move_verblijf");
+                }
+                hint3.onended = () => {
+                    mouthMove.style.display = "none";
+                    startOKBtn.style.display = "block";
+                    herhaal.style.display = "block";
+                }
+                image = '../images/savanne/keyboard.png';
+                startOKBtn.onclick = () => {
+                    speechBubble.style.visibility = "hidden";
+                    explaineBtn.classList.add("hide");
+                    speechBubble_p.style.visibility = "hidden";
+                    isHint = false;
+                    // startOKBtn.style.visibility = "hidden";
+                    countHint++;
+                }
+                break;
+            case 3: 
+                speechBubble.style.visibility = "visible";
+                speechBubble_p.style.visibility = "visible";
+                startOKBtn.style.visibility = "visible";
+                tekst = 'Het antwoord is olifant.';
+                hint4.play();
+                hint4.onplaying = () => {
+                    mouthMove.style.display = "block";
+                    mouthMove.classList.add("mouth_move_verblijf");
+                }
+                hint4.onended = () => {
+                    mouthMove.style.display = "none";
+                    startOKBtn.style.display = "block";
+                    herhaal.style.display = "block";
+                }
+                startOKBtn.onclick = () => {
+                    speechBubble.style.visibility = "hidden";
+                    speechBubble_p.style.visibility = "hidden";
+                    startOKBtn.style.visibility = "hidden";
+                    isHint = false;
+                    countHint++;
+                }
+                break;
+            case 4:
                 speechBubble.style.visibility = "hidden";
-                explaineBtn.classList.add("hide");
-                speechBubble_p.style.visibility = "hidden";
-                isHint = false;
+                startOKBtn.style.display = "none";
                 // startOKBtn.style.visibility = "hidden";
                 countHint++;
-            }
-            break;
-        case 3: 
-            speechBubble.style.visibility = "visible";
-            speechBubble_p.style.visibility = "visible";
-            startOKBtn.style.visibility = "visible";
-            tekst = 'Het antwoord is olifant.';
-            hint4.play();
-            hint4.onplaying = () => {
-                mouthMove.style.display = "block";
-                mouthMove.classList.add("mouth_move_verblijf");
-            }
-            hint4.onended = () => {
-                mouthMove.style.display = "none";
-                startOKBtn.style.display = "block";
-                herhaal.style.display = "block";
-            }
-            startOKBtn.onclick = () => {
-                speechBubble.style.visibility = "hidden";
-                speechBubble_p.style.visibility = "hidden";
-                startOKBtn.style.visibility = "hidden";
-                isHint = false;
-                countHint++;
-            }
-            break;
-        case 4:
-            speechBubble.style.visibility = "hidden";
-            startOKBtn.style.display = "none";
-            // startOKBtn.style.visibility = "hidden";
-            countHint++;
-            break;
-    }
-    
-    herhaal.style.display = "none";
-    startOKBtn.style.display = "none";
-    speechBubble_p.innerHTML = tekst;
-    explaineBtn.src = image;
-    
+                break;
+        }
+        
+        herhaal.style.display = "none";
+        startOKBtn.style.display = "none";
+        speechBubble_p.innerHTML = tekst;
+        explaineBtn.src = image;
+        
+    };
 };
-
-function endRekensomPuzzel(){
-    window.location.href="dierentuinpad.html";  
-}
-
+  
+export { countHint, startTime};
