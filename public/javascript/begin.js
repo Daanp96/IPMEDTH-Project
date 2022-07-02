5eb535f6654d45132fa60c5a1a6e87b5621b38f3
s1107777 <s1107777@student.hsleiden.nl>
2022-07-02 11:32:05 +0200

    ijswinkel bugs gefixed

--- public/javascript/begin.js
+++ public/javascript/begin.js
@@ -1,14 +1,11 @@
+import {reloadSpeech} from "./functions.js";
+
 let welkeKlas = new Audio("./audio/Tjalle/0-keuzescherm/1-welkeklas.m4a");
 
-const speechBubble = document.getElementById("js--speech-bubble");
-const startExplaineBtn = document.getElementById("js--speech-bubble-img");
-const startP = document.getElementById("js--speech-bubble-p");
 const mouthMove = document.getElementById("js--mouth");
+const speechReload = document.getElementById("js--speech-reload");
 
 const groep3 = document.getElementById("js--btn-groep3");
-
-let audioHerhaal = [welkeKlas];
-let countHerhaal = 0;
 
 welkeKlas.play();
 welkeKlas.onplaying = () => {
@@ -17,8 +14,7 @@ welkeKlas.onplaying = () => {
 }
 welkeKlas.onended = () => {
     mouthMove.style.display = "none";
-    // startOKBtn.style.display = "block";
-    // speechReload.style.display = "block";
+    speechReload.style.display = "block";
 }
 
 groep3.onclick = () =>{
@@ -27,8 +23,6 @@ groep3.onclick = () =>{
 
 window.localStorage.clear();
 
-// speechReload.onclick = () => {
-//     reloadSpeech(audioHerhaal[countHerhaal], speechReload);
-// }
-
-
+speechReload.onclick = () => {
+    reloadSpeech(welkeKlas, speechReload, mouthMove);
+}
