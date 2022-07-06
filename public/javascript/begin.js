eaf2d101bd62ab1a62131dece8cd5b3f9d0871fd
Daan <daan.passchier@xs4all.nl>
2022-07-06 10:15:30 +0200

    begin knop en overlay

--- public/javascript/begin.js
+++ public/javascript/begin.js
@@ -3,21 +3,28 @@ import {reloadSpeech} from "./functions.js";
 let welkeKlas = new Audio("./audio/Tjalle/0-keuzescherm/1-welkeklas.m4a");
 
 const speechReload = document.getElementById("js--speech-reload");
+const beginBtn = document.getElementById("js--begin");
+const overlay = document.getElementById("js--begin-overlay");
 
 const groep2 = document.getElementById("js--btn-groep2");
 const groep3 = document.getElementById("js--btn-groep3");
 
-welkeKlas.play();
-
-welkeKlas.onended = () => {
-    speechReload.style.display = "block";
-}
 groep2.onclick = () =>{
     window.location = "pages/groep-2/start.html"
 };
 groep3.onclick = () =>{
     window.location = "pages/start.html"
 };
+
+beginBtn.onclick = () => {
+    overlay.style.opacity = "0";
+    overlay.style.zIndex = "-1";
+    welkeKlas.play();
+
+    welkeKlas.onended = () => {
+        speechReload.style.display = "block";
+    }
+}
 
 window.localStorage.clear();
 
